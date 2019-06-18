d242646b71eb49ac89780f23ec7f3184f0bf0100
jaroslawrutk <jaroslaw.rutk@gmail.com>
2019-06-18 20:27:23 +0200

    Dodaj pliki projektów.

---
+++ SchoolStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
